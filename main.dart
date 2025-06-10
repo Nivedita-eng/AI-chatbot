94f08c4a76f4408ec635bf71b7fe19d4752c88b9
Nivedita-eng <niveditakanwar159@gmail.com>
2025-06-10 17:30:34 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+
+import 'package:chatbot/pages/home_page.dart';
+import 'package:chatbot/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatbot',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor:AppColors.submitButton ),
+        textTheme: GoogleFonts.interTextTheme(
+          ThemeData.dark().textTheme,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+
+
